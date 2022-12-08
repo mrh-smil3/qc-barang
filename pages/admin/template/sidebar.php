275042398187464e32b1ff95361bf0474c0d8330
ARIF <50254637+mrh-smil3@users.noreply.github.com>
2022-12-08 10:56:05 +0700

    Add files via upload

--- pages/admin/template/sidebar.php
+++ pages/admin/template/sidebar.php
@@ -7,7 +7,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Page Dasboard</title>
+	<title>Page QC</title>
 	<!--     Fonts and icons     -->
 	  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
 	  <!-- Nucleo Icons -->
@@ -24,7 +24,7 @@
   <aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3   bg-gradient-dark" id="sidenav-main">
     <div class="sidenav-header">
       <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
-      <a class="navbar-brand m-0" href=" https://demos.creative-tim.com/material-dashboard/pages/dashboard " target="_blank">
+      <a class="navbar-brand m-0" href="dashboard.php" >
         <img src="../../assets/img/logo-ct.png" class="navbar-brand-img h-100" alt="main_logo">
         <span class="ms-1 font-weight-bold text-white">Quality Control</span>
       </a>
